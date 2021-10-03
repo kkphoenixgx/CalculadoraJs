331e85d016d303dbec85ff5d0ed173c5c66f5280
Kauã Alves <kauaAlvesWorkplace@gmail.com>
2021-10-03 18:08:42 -0300

    changed the variable names for private

--- js/Controller/controller.js
+++ js/Controller/controller.js
@@ -1,4 +1,7 @@
+// @collapse
+
 class Controller{
+
     constructor(){
         //Buttons
         this.numberButtons = document.querySelectorAll('[data-number]');
@@ -15,278 +18,280 @@ class Controller{
         this.initButtonEvents();
 
         // operation
-        this.operation = [];
-        this.newOperation = [];
+        this._operation = [];
+        this._newOperation = [];
         this.lastNumber = '';
         this.lastOperator = '';
     }
+
     //baseFunctions->
 
-    initButtonEvents(){
-        this.numberButtons.forEach(btn => {
-            btn.addEventListener('click', () => {
-                this.execute(btn.innerText);
+        initButtonEvents(){
+            this.numberButtons.forEach(btn => {
+                btn.addEventListener('click', () => {
+                    this.execute(btn.innerText);
+                })
+            });
+            this.operatorButtons.forEach(operator => {
+                operator.addEventListener('click', () => {
+                    this.execute(operator.innerText);
+                })
+            });
+
+            this.ACButton.addEventListener('click', () => {
+                this.execute(this.ACButton.innerText);
+            });
+            this.equalButton.addEventListener('click', () => {
+                this.execute(this.equalButton.innerText);
+            });
+            this.CeButton.addEventListener('click', () => {
+                this.execute(this.CeButton.innerText);
             })
-        });
-        this.operatorButtons.forEach(operator => {
-            operator.addEventListener('click', () => {
-                this.execute(operator.innerText);
-            })
-        });
 
-        this.ACButton.addEventListener('click', () => {
-            this.execute(this.ACButton.innerText);
-        });
-        this.equalButton.addEventListener('click', () => {
-            this.execute(this.equalButton.innerText);
-        });
-        this.CeButton.addEventListener('click', () => {
-            this.execute(this.CeButton.innerText);
-        })
+            document.addEventListener('keyup', e =>{
 
-        document.addEventListener('keyup', e =>{
+                switch(e.key){
 
-            switch(e.key){
+                    case 'Escape':
+                        this.execute('AC');
+                        break;
+                    case 'Backspace':
+                        console.log(e.key);
+                        break;
+                    case '+':
+                        this.addOperation('+');
+                    case '-':
+                        this.addOperation('-');
+                    case '/':
+                        this.addOperation('/');
+                    case '*':
+                        this.addOperation('*');
+                    case 'Enter':
+                    case '=':
+                        console.log(e.key);
+                        break;
+                    case '.':
+                    case ',':
+                        console.log(e.key);
+                        break;
+                    case 'c':
+                        console.log(e.key); 
+                        break;
+                    case '0':
+                    case '1':
+                    case '2':
+                    case '3':
+                    case '4':
+                    case '5':
+                    case '6':
+                    case '7':
+                    case '8':
+                    case '9':
+                        this.addOperation(parseInt(e.key));
+                        break;
+                }
+            
+            });
+        }
 
-                case 'Escape':
-                    this.execute('AC');
-                    break;
-                case 'Backspace':
-                    console.log(e.key);
-                    break;
-                case '+':
-                    this.addOperation('+');
-                case '-':
-                    this.addOperation('-');
-                case '/':
-                    this.addOperation('/');
-                case '*':
-                    this.addOperation('*');
-                case 'Enter':
-                case '=':
-                    console.log(e.key);
-                    break;
-                case '.':
-                case ',':
-                    console.log(e.key);
-                    break;
-                case 'c':
-                    console.log(e.key); 
-                    break;
+    //Operations->
+
+        addOperation(value){
+
+            if(isNaN(this.getLastOperation())){
+            if(this.isOperator(value)){
+                    console.log(value);
+                    if(this._newOperation.length == 0){
+                        this.pushOperation(value);
+                    }else{
+                        this.setLastOperatorOperation(value);
+                        this.updateCurrentLog();
+                    }
+            }else{
+                    console.log(value);
+                    this.pushOperation(value);
+            }
+            }else{
+                if(this.isOperator(value)){
+                    this.pushOperation(value);
+                }else{
+                    let newValue = this.getLastOperation().toString() + value.toString();
+                    this.setLastOperation(newValue);
+                    this.updateDisplay();
+                }
+            }
+        }
+
+        addDot(){
+            let lastOperation = this.getLastOperation();
+
+            if (typeof lastOperation === 'string' && lastOperation.split('').indexOf('.') > -1) return;
+
+            if(this.isOperator(lastOperation) || !lastOperation){
+                this.pushOperation('0.');
+            }else{
+                this.setLastOperation(lastOperation.toString() + '.');
+            }
+            this.updateDisplay();
+        }
+
+        calculate(){
+            let lastDigit = this._newOperation.pop();
+
+            let result = eval(this._newOperation.join(""));
+
+            this._newOperation = [result,lastDigit];
+            this.logScreenTextElement.innerText = result;
+            this.updateCurrentLog();
+            this.updateDisplay();
+        }
+
+        equal(){
+            let calculate = this._newOperation.join("") + this._operation.join("");
+            this._operation = [eval(calculate)];
+            this._newOperation = [];
+            this.updateCurrentLog();
+            this.updateDisplay();
+        }
+
+        CE(){
+            if(this._operation.length =! 0 && this._operation	== 0){
+                this._newOperation.pop(this._newOperation.length-1);
+                this.updateCurrentLog();
+            }else{
+                this._operation = [];
+                this.updateDisplay();
+            }
+        }
+
+        execute(value){
+            switch(value){
+
                 case '0':
                 case '1':
                 case '2':
                 case '3':
                 case '4':
-                case '5':
+                case '5':            
                 case '6':
                 case '7':
                 case '8':
                 case '9':
-                    this.addOperation(parseInt(e.key));
+                    this.addOperation(parseInt(value));
                     break;
+                    case '.':
+                        this.addDot();
+                        break;
+                case '*':
+                    console.log(value);
+                    this.addOperation('*');
+                    break;
+                case '/':
+                    console.log(value);
+                    this.addOperation('/');
+                    break;
+                case '+':
+                    console.log(value);
+                    this.addOperation('+');
+                    break;
+                case '-':
+                    console.log(value);
+                    this.addOperation('-');
+                    break;
+
+                case 'AC':
+                    this.clearScreen();
+                    break;
+                case 'CE':
+                    this.CE();
+                    break;
+                case '=':
+                    this.equal();
+                default:
+                    this.setAnError();
+                    break;
+                
             }
-        
-        });
-    }
+        }
 
     //Methods->
+
         //screen
 
-        clearScreen(){
-            this.currentLog = '';
-            this.operation = [];
-            this.newOperation = [];
-            this.logScreen = '0';
-            this.currentLog = '0';
-            this.updateDisplay();
-            this.updateCurrentLog();
-        }
-        updateDisplay(){
-            if(this.operation == 0){
-                this.logScreenTextElement.innerText = '0';
-            }else{
-                this.logScreen = this.operation.toString();
-                this.logScreenTextElement.innerText = this.logScreen;
+            clearScreen(){
+                this.currentLog = '';
+                this._operation = [];
+                this._newOperation = [];
+                this.logScreen = '0';
+                this.currentLog = '0';
+                this.updateDisplay();
+                this.updateCurrentLog();
             }
-        }
-        updateCurrentLog(){
-           
-            let operationToLog = this.newOperation.toString().replace(/,/g , " ");
+            updateDisplay(){
+                if(this._operation == 0){
+                    this.logScreenTextElement.innerText = '0';
+                }else{
+                    this.logScreen = this._operation.toString();
+                    this.logScreenTextElement.innerText = this.logScreen;
+                }
+            }
+            updateCurrentLog(){
+            
+                let operationToLog = this._newOperation.toString().replace(/,/g , " ");
 
-            this.currentLog = operationToLog.toString();
-            this.currentLogTextElement.innerText = this.currentLog;
-         
-        }
-
-        setAnError(){
-            this.logScreen = "ERROR";
-        }
+                this.currentLog = operationToLog.toString();
+                this.currentLogTextElement.innerText = this.currentLog;
+            
+            }
+            setAnError(){
+                this.logScreen = "ERROR";
+            }
 
         //operations
 
-        isOperator(value){
-            return (['+' , '-', '*', '/'].indexOf(value) > -1)
-        }
-        pushOperation(value){
-            console.log(value);
-            if(this.isOperator(value)){
-                //quando é um operador
-                console.log(value);
-                this.newOperation.push(this.operation);
-                this.newOperation.push(value);
-                this.updateCurrentLog();
-                this.operation = [];
-                if(this.newOperation.length >= 3){
-                    console.log(this.newOperation);
-                    this.calculate();
-                }
-                this.updateDisplay();
-
-            }else{
-                // onde o número normal entra
-                console.log(value);
-                this.operation.push(value);
-                this.updateDisplay();
-                this.updateCurrentLog();
+            isOperator(value){
+                return (['+' , '-', '*', '/'].indexOf(value) > -1)
             }
-        }
+            pushOperation(value){
+                console.log(value);
+                if(this.isOperator(value)){
+                    //quando é um operador
+                    console.log(value);
+                    this._newOperation.push(this._operation);
+                    this._newOperation.push(value);
+                    this.updateCurrentLog();
+                    this._operation = [];
+                    if(this._newOperation.length >= 3){
+                        console.log(this._newOperation);
+                        this.calculate();
+                    }
+                    this.updateDisplay();
+
+                }else{
+                    // onde o número normal entra
+                    console.log(value);
+                    this._operation.push(value);
+                    this.updateDisplay();
+                    this.updateCurrentLog();
+                }
+            }
 
         //gets
 
-        getLastItem(){
-            return this.operation[this.operation.length -1];
-        }
-        getLastOperation(){
-            return this.operation[this.operation.length -1];
-        }
+            getLastItem(){
+                return this._operation[this._operation.length -1];
+            }
+            getLastOperation(){
+                return this._operation[this._operation.length -1];
+            }
 
         //sets
 
-        setLastOperation(value){
-            this.operation[this.operation.length -1] = value;
-        }
-        setLastOperatorOperation(value){
-            this.newOperation[this.newOperation.length-1] = value;
-        }
-
-    //Operations->
-
-    addOperation(value){
-
-        if(isNaN(this.getLastOperation())){
-           if(this.isOperator(value)){
-                console.log(value);
-                if(this.newOperation.length == 0){
-                    this.pushOperation(value);
-                }else{
-                    this.setLastOperatorOperation(value);
-                    this.updateCurrentLog();
-                }
-           }else{
-                console.log(value);
-                this.pushOperation(value);
-           }
-        }else{
-            if(this.isOperator(value)){
-                this.pushOperation(value);
-            }else{
-                let newValue = this.getLastOperation().toString() + value.toString();
-                this.setLastOperation(newValue);
-                this.updateDisplay();
+            setLastOperation(value){
+                this._operation[this._operation.length -1] = value;
             }
-        }
-    }
+            setLastOperatorOperation(value){
+                this._newOperation[this._newOperation.length-1] = value;
+            }
 
-    addDot(){
-        let lastOperation = this.getLastOperation();
-
-        if (typeof lastOperation === 'string' && lastOperation.split('').indexOf('.') > -1) return;
-
-        if(this.isOperator(lastOperation) || !lastOperation){
-            this.pushOperation('0.');
-        }else{
-            this.setLastOperation(lastOperation.toString() + '.');
-        }
-        this.updateDisplay();
-    }
-
-    calculate(){
-        let lastDigit = this.newOperation.pop();
-
-        let result = eval(this.newOperation.join(""));
-
-        this.newOperation = [result,lastDigit];
-        this.logScreenTextElement.innerText = result;
-        this.updateCurrentLog();
-        this.updateDisplay();
-    }
-
-    equal(){
-        let calculate = this.newOperation.join("") + this.operation.join("");
-        this.operation = [eval(calculate)];
-        this.newOperation = [];
-        this.updateCurrentLog();
-        this.updateDisplay();
-    }
-
-    CE(){
-        if(this.operation.length =! 0 && this.operation	== 0){
-            this.newOperation.pop(this.newOperation.length-1);
-            this.updateCurrentLog();
-        }else{
-            this.operation = [];
-            this.updateDisplay();
-        }
-    }
-
-    execute(value){
-        switch(value){
-
-            case '0':
-            case '1':
-            case '2':
-            case '3':
-            case '4':
-            case '5':            
-            case '6':
-            case '7':
-            case '8':
-            case '9':
-                this.addOperation(parseInt(value));
-                break;
-                case '.':
-                    this.addDot();
-                    break;
-            case '*':
-                console.log(value);
-                this.addOperation('*');
-                break;
-            case '/':
-                console.log(value);
-                this.addOperation('/');
-                break;
-            case '+':
-                console.log(value);
-                this.addOperation('+');
-                break;
-            case '-':
-                console.log(value);
-                this.addOperation('-');
-                break;
-
-            case 'AC':
-                this.clearScreen();
-                break;
-            case 'CE':
-                this.CE();
-                break;
-            case '=':
-                this.equal();
-            default:
-                this.setAnError();
-                break;
-            
-        }
-    }
 }
